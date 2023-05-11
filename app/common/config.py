d784177094b39bc5c0a87f562bcb498e54aae79c
nikola <nikola@vlending.co.kr>
2023-05-11 15:45:27 +0900

    환경 설정 class 수정

--- app/common/config.py
+++ app/common/config.py
@@ -6,23 +6,42 @@ base_dir = path.dirname(path.dirname(path.dirname(path.abspath(__file__))))
 
 @dataclass
 class Config:
-    BASE_DIR = base_dir
+    """
+    기본 Configuration
+    """
+    BASE_DIR: str = base_dir
     DB_POOL_RECYCLE: int = 900
     DB_ECHO: bool = True
-    DB_URL: str = None
+    DEBUG: bool = False
+    TEST_MODE: bool = False
 
 
 @dataclass
 class LocalConfig(Config):
-    PROJ_RELOAD: bool = True
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    DEBUG: bool = True
     DB_URL: str = field(default='mysql+pymysql://root:adsf25did@localhost:13306/fastapi')
 
 
 @dataclass
 class ProdConfig(Config):
-    PROJ_RELOAD: bool = False
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+
+
+@dataclass
+class TestConfig(Config):
+    DB_URL: str = "mysql+pymysql://root:adsf25did@localhost:13306/fastapi"
+    TRUSTED_HOSTS = ["*"]
+    ALLOW_SITE = ["*"]
+    TEST_MODE: bool = True
 
 
 def conf():
-    config = dict(prod=ProdConfig(), local=LocalConfig())
-    return config.get(environ.get('API_ENV', 'local'))
+    """
+    환경 불러오기
+    :return:
+    """
+    config = dict(prod=ProdConfig, local=LocalConfig, test=TestConfig)
+    return config[environ.get("API_ENV", "local")]()
